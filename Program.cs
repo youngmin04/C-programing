448692e5e5699b5880c00dee79dca5f45617cdc1
youngmin04 <s20067@gsm.hs.kr>
2021-03-11 10:35:02 +0900

    C#part1

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using static System.Console;
+
+namespace Programing
+{
+    class Program
+    {
+        //나는 주석이다
+        /*나는 주석이다*/
+        static void Main(string[] args)
+        {
+            int a = 100;
+
+            WriteLine("와! 응용프로그램 첫 시간!!");
+        }
+    }
+}
