ca9eeeeedd46273623ad0a20bed1fa6cc06bd086
youngmin04 <s20067@gsm.hs.kr>
2021-03-14 17:10:02 +0900

    C#part3

---
+++ String.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programing
+{
+    class String
+    {
+        static void Main(string[] args)
+        {
+            string a = "안녕하세요" + " 반갑습니다." + " 정영민입니다.";
+            string b = " 반갑습니다.";
+
+            Console.WriteLine(a);
+        }
+    }
+}
