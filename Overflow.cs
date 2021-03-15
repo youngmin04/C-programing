889e80a0132903e25fad2ae94b102a853ad259a0
youngmin04 <s20067@gsm.hs.kr>
2021-03-16 08:09:06 +0900

    C#part5

---
+++ Overflow.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static.System.Console;
+
+namespace Programing
+{
+    class Overflow
+    {
+        static void Main(string[] args)
+        {
+            uint a = uint.MaxValue;
+
+            WriteLine(a);
+
+            a = a + 1;
+
+            WriteLine(a);
+        }
+    }
+}
---
+++ SignedUnsigned.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programing
+{
+    class SignedUnsigned
+    {
+        static void Main()
+        {
+            byte a = 255;
+            sbyte b = (sbyte)a;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+}
