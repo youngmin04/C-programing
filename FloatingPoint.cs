2c0e63c926d05b34a7674709b080138b9346d47c
youngmin04 <s20067@gsm.hs.kr>
2021-03-17 08:08:44 +0900

    C#part6

---
+++ FloatingPoint.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programing
+{
+    class FloatingPoint
+    {
+        static void Main(string[] args)
+        {
+            float a = 3.14159265358979323846f;
+            Console.WriteLine(a);
+
+            double b = 3.14159265358979323846;
+            Console.WriteLine(b);
+        }
+    }
+}
