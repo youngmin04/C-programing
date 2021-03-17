e3af48201dad3b9276877ed7ff59268e4721d25a
youngmin04 <s20067@gsm.hs.kr>
2021-03-18 07:44:00 +0900

    C#part7

---
+++ Decimal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programing
+{
+    class Decimal
+    {
+        static void Main(String[] args)
+        {
+            float a = 3.141592653589793238462643383279f;
+            double b = 3.141592653589793238462643383279;
+            decimal c = 3.141592653589793238462643383279m;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+        }
+    }
+}
