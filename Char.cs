8dca61689c499393448309f88e000a136f7b53cb
youngmin04 <s20067@gsm.hs.kr>
2021-03-19 07:45:23 +0900

    C#part8

---
+++ Char.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programing
+{
+    class Char
+    {
+        static void Main()
+        {
+            char a = '안';
+            char b = '녕';
+            char c = '하';
+            char d = '세';
+            char e = '요';
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+        }
+    }
+}
