2cc43f0beb65a3dcabb218fd9992ed802340adbe
youngmin04 <s20067@gsm.hs.kr>
2021-03-20 20:59:19 +0900

    C#part9

---
+++ Bool.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programing
+{
+    class Bool
+    {
+        static void Main(string[] args)
+        {
+            bool a = true;
+            bool b = false;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+}
