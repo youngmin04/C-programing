527bdd1e82eeaf46b5fccbe9f8b54e16f39ae8ea
youngmin04 <s20067@gsm.hs.kr>
2021-03-22 07:47:27 +0900

    C#part11

---
+++ BoxingUnboxing.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programing
+{
+    class BoxingUnboxing
+    {
+        static void Main(string[] args)
+        {
+            int a = 123;
+            object b = (object)a;
+            int c = (int)b;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+
+            double x = 3.1414213;
+            double y = x;
+            double z = (double)y;
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+        }
+    }
+}
