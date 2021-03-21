c2001281b02309e9b8691563e76dfe46ef4d9ba5
youngmin04 <s20067@gsm.hs.kr>
2021-03-21 20:40:35 +0900

    C#part10

---
+++ Objcect.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programing
+{
+    class Objcect
+    {
+        static void Main(string[] args)
+        {
+            object a = 123;
+            object b = 3.141592653589793238462643383279m;
+            object c = true;
+            object d = "안녕하세요";
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+        }
+    }
+}
