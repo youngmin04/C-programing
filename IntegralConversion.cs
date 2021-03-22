6d78c35b90ec9e9d2271d4bfb4769bc76895442a
youngmin04 <s20067@gsm.hs.kr>
2021-03-23 07:43:31 +0900

    C#part11

---
+++ IntegralConversion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programing
+{
+    class IntegralConversion
+    {
+        static void Main(string[] arg)
+        {
+            sbyte a = 127;
+            Console.WriteLine(a);
+
+            int b = (int)a;
+            Console.WriteLine(b);
+
+            int x = 128;
+            Console.WriteLine(x);
+
+            sbyte y = (sbyte)x;
+            Console.WriteLine(y);
+        }
+    }
+}
