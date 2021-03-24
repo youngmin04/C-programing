00a16fc6ed2c330f8c1d6fd3f58f2db19c83e4df
youngmin04 <s20067@gsm.hs.kr>
2021-03-25 07:42:58 +0900

    C#part14

---
+++ SignedUnsignedConversion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programing
+{
+    class SignedUnsignedConversion
+    {
+        static void Main(string[] args)
+        {
+            int a = 500;
+            Console.WriteLine(a);
+
+            uint b = (uint)a;
+            Console.WriteLine(b);
+
+            int x = -30;
+            Console.WriteLine(x);
+
+            uint y = (uint)x;
+            Console.WriteLine(y);
+        }
+    }
+}
