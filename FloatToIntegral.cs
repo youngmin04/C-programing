d901bcc52b4a634a36223b9b950409ae95da0ae6
youngmin04 <s20067@gsm.hs.kr>
2021-03-29 07:41:48 +0900

    C#part15

--- FloatToIntegral.cs
+++ FloatToIntegral.cs
@@ -6,5 +6,15 @@ namespace Programing
 {
     class FloatToIntegral
     {
+        static void Main(string[] args)
+        {
+            float a = 0.9f;
+            int b = (int)a;
+            Console.WriteLine(b);
+
+            float c = 1.1f;
+            int d = (int)c;
+            Console.WriteLine(d);
+        }
     }
 }
