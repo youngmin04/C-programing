3b992c34ab391f1a453f15f6bfa05ec610a1e9da
youngmin04 <s20067@gsm.hs.kr>
2021-03-29 07:43:09 +0900

    C#part17

---
+++ StringNumberConverion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programing
+{
+    class StringNumberConverion
+    {
+        static void Main()
+        {
+            int a = 123;
+            string b = a.ToString();
+            Console.WriteLine(b);
+
+            float c = 3.14f;
+            string d = c.ToString();
+            Console.WriteLine(d);
+
+            string e = "123456";
+            int f = Convert.ToInt32(e);
+            Console.WriteLine(f);
+
+            string g = "1.2345";
+            float h = float.Parse(g);
+            Console.WriteLine(h);
+        }
+    }
+}
