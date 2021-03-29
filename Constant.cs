b626d489fffc4d3ab08b69b92e019c0d0b9b7510
youngmin04 <s20067@gsm.hs.kr>
2021-03-30 07:40:32 +0900

    C#part18

---
+++ Constant.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programing
+{
+    class Constant
+    {
+        static void Main()
+        {
+            const int a = int.MaxValue;
+            const int b = int.MinValue;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+            //a = 3; // 변수값은 변경할 수 없다 그렇기에 오류 발생
+        }
+    }
+}
