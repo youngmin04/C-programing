4f0fb821a3ba2aee53ba52fbe6f47d48a21b3158
youngmin04 <s20067@gsm.hs.kr>
2021-03-31 07:38:25 +0900

    C#part18

---
+++ Enum02.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programing
+{
+    class Enum02
+    { 
+        enum DialogResult { Yes,No,Cancel,Confirm,Ok};
+        static void Main()
+        {
+            DialogResult result = DialogResult.Yes;
+
+            Console.WriteLine(result == DialogResult.Yes);
+            Console.WriteLine(result == DialogResult.No);
+            Console.WriteLine(result == DialogResult.Cancel);
+            Console.WriteLine(result == DialogResult.Confirm);
+            Console.WriteLine(result == DialogResult.Ok);
+        }
+    }
+}
