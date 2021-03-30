032dac179904e5ba12ee27e80ae0329eb30de15c
youngmin04 <s20067@gsm.hs.kr>
2021-03-31 07:38:04 +0900

    C#part18

---
+++ Enum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programing
+{
+    class Constact
+    {
+        enum DialogResult {Yes,No,Cancel,Confirm,Ok  }
+        static void Main(string[] args)
+        {
+            Console.WriteLine((int)DialogResult.Yes);
+            Console.WriteLine((int)DialogResult.No);
+            Console.WriteLine((int)DialogResult.Cancel);
+            Console.WriteLine((int)DialogResult.Confirm);
+            Console.WriteLine((int)DialogResult.Ok);
+        }
+    }
+}
