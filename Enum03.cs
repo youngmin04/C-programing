910c3f9e84b9e6d1e75c0a28d60a593062c1aec4
youngmin04 <s20067@gsm.hs.kr>
2021-04-01 07:41:29 +0900

    C#part19

---
+++ Enum03.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programing
+{
+    class Enum03
+    {
+        enum DialogResult { Yes=10,No,Cancel,Confirm=50,Ok};
+
+        static void Main()
+        {
+            Console.WriteLine((int)DialogResult.Yes);
+            Console.WriteLine((int)DialogResult.No);
+            Console.WriteLine((int)DialogResult.Cancel);
+            Console.WriteLine((int)DialogResult.Confirm);
+            Console.WriteLine((int)DialogResult.Ok);
+        }
+    }
+}
