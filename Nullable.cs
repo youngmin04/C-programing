fbc1e883302e2093c176572ce08f56ba4958f0a0
youngmin04 <s20067@gsm.hs.kr>
2021-04-02 07:41:30 +0900

    C#part19

---
+++ Nullable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programing
+{
+    class Nullable
+    {
+        static void Main(string[] args)
+        {
+            int? a = null;
+
+            Console.WriteLine(a.HasValue);
+            Console.WriteLine(a!=null);
+
+            a = 3;
+
+            Console.WriteLine(a.HasValue);
+            Console.WriteLine(a != null);
+            Console.WriteLine(a.Value);
+        }
+    }
+}
