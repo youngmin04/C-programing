dafef539d098837444eafc5dbe1e03ea918b94d2
youngmin04 <s20067@gsm.hs.kr>
2021-04-03 22:51:53 +0900

    C#part21

---
+++ Cts.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programing
+{
+    class Cts
+    {
+        static void Main()
+        {
+            System.Int32 a = 123;
+            int b = 456;
+
+            Console.WriteLine("a type : {0}, value : {1}", a.GetType(), a);
+            Console.WriteLine("b type : {0}, value : {1}", b.GetType(), b);
+
+            System.String c = "abc";
+            string d = "def";
+
+            Console.WriteLine("c type : {0}, value : {1}", c.GetType(), c);
+            Console.WriteLine("d type : {0}, value : {1}", d.GetType(), d);
+        }
+    }
+}
