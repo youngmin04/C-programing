dd02db1777771349adf0547aa3b4bae2b046c358
youngmin04 <s20067@gsm.hs.kr>
2021-04-06 07:46:02 +0900

    C#part23

---
+++ StringSlice.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Console;
+
+namespace Programing
+{
+    class StringSlice
+    {
+        static void Main(string[] args)
+        {
+            string greeting = "Good morning.";
+
+            WriteLine(greeting.Substring(0, 5));
+            WriteLine(greeting.Substring(5));
+            WriteLine();
+
+            string[] arr = greeting.Split(
+                new string[] { "" }, StringSplitOptions.None);
+            WriteLine("Word Count : {0}", arr.Length);
+
+            foreach (string element in arr)
+                WriteLine("{0}", element);
+        }
+    }
+}
