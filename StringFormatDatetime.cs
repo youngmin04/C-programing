2e6005a48f1264a03bac5a152607cfe32fad54ca
youngmin04 <s20067@gsm.hs.kr>
2021-04-09 07:51:09 +0900

    C#part27

---
+++ StringFormatDatetime.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+using static System.Console;
+
+namespace Programing
+{
+    class StringFormatDatetime
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2018, 11, 3, 23, 18, 22);
+
+            WriteLine("12시간 형식 : {0:yyyy-MM-dd tt hh:mm:ss (ddd)}", dt);
+            WriteLine("24시간 형식 : {0:yyyy-MM-dd HH:mm:ss (ddd)}", dt);
+
+            CultureInfo ciko = new CultureInfo("ko-KR");
+
+            WriteLine();
+            WriteLine(dt.ToString("yyyy-MM-dd tt hh:mm:ss (ddd)", ciko));
+            WriteLine(dt.ToString("yyyy-MM-dd HH:mm:ss (ddd)", ciko));
+            WriteLine(dt.ToString(ciko));
+
+            CultureInfo ciEn = new CultureInfo("en-Us");
+
+            WriteLine();
+            WriteLine(dt.ToString("yyyy-MM-dd tt hh:mm:ss (ddd)", ciEn));
+            WriteLine(dt.ToString("yyyy-MM-dd HH:mm:ss (ddd)", ciEn));
+            WriteLine(dt.ToString(ciEn));
+        }
+    }
+}
