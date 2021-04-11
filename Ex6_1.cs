fb846f493e540ec4a98d98be4e7cabe532a3008b
youngmin04 <s20067@gsm.hs.kr>
2021-04-12 07:40:25 +0900

    C#part27

---
+++ Ex6_1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programing
+{
+    class Ex6_1
+    {
+        static double Square(double arg)
+        {
+            return arg * arg;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("수를 입력하세요 : ");
+            string input = Console.ReadLine();
+            double arg = Convert.ToDouble(input);
+
+            Console.WriteLine("결과 : {0}", Square(arg));
+        }
+    }
+}
