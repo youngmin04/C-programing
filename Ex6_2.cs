83ad524620a1333442b0661366ad820e4a0acc69
youngmin04 <s20067@gsm.hs.kr>
2021-04-12 07:41:02 +0900

    C#part27

---
+++ Ex6_2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programing
+{
+    class Ex6_2
+    {
+        public static void Main()
+        {
+            double mean = 0;
+
+            Mean(1,2, 3, 4, 5, ref mean);
+
+            Console.WriteLine("평균 : {0}", mean);
+        }
+
+        public static void Mean(double a,double b,double c,
+            double d, double e, ref double mean)
+        {
+            mean = (a + b + c + d + e) / 5;
+        }
+
+    }
+}
