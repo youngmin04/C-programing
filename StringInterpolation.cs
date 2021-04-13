f7212b098641f209fa958a6bb777c44d447c10a7
youngmin04 <s20067@gsm.hs.kr>
2021-04-14 07:46:45 +0900

    C#part29

---
+++ StringInterpolation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Console;
+
+namespace Programing
+{
+    class StringInterpolation
+    {
+        static void Main()
+        {
+            string name = "김튼튼";
+            int age = 23;
+            WriteLine($"{name,-10},{age:D3}");
+
+            name = "박날씬";
+            age = 30;
+            WriteLine($"{name},{age,-10:D3}");
+
+            name = "이비실";
+            age = 17;
+            WriteLine($"{name},{(age>20?"성인":"미성년자")}");
+        }
+    }
+}
