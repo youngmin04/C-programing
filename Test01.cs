cb14f3042c5d3f1938781b753c817fed60d05cba
youngmin04 <s20067@gsm.hs.kr>
2021-04-15 07:41:55 +0900

    C#part30

---
+++ Test01.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programing
+{
+    class Test01
+    {
+        static void Main()
+        {
+            Console.WriteLine("사각형의 너비를 입력하세요.");
+            string width = Console.ReadLine();
+
+            Console.WriteLine("사각형의 높이를 입력하세요.");
+            string height = Console.ReadLine();
+
+            int w = int.Parse(width);
+            int h = int.Parse(height);
+
+            Console.WriteLine("사각형의 넓이는 : " + w * h);
+        }
+    }
+}
