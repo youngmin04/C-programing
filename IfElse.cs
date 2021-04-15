38da55a238ab77db053f244f138b732c1c213628
youngmin04 <s20067@gsm.hs.kr>
2021-04-16 07:52:36 +0900

    C#part31

---
+++ IfElse.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Console;
+
+namespace Programing
+{
+    class IfElse
+    {
+        static void Main()
+        {
+            Write("숫자를 입력하세요. : ");
+
+            string input = ReadLine();
+            int number = Int32.Parse(input);
+
+            if (number < 0)
+            {
+                WriteLine("음수");
+            }
+            else if(number>0)
+            {
+                WriteLine("양수");
+            }
+            else
+            {
+                WriteLine("0");
+            }
+
+            if (number % 2 == 0)
+            {
+                WriteLine("짝수");
+            }
+            else
+            {
+                WriteLine("홀수");
+            }
+        }
+    }
+}
