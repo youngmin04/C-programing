d937d8fbdc4a9323513e13da8c3631e6358a93d4
youngmin04 <s20067@gsm.hs.kr>
2021-04-20 07:38:53 +0900

    C#part34

---
+++ ArithmaticOperators.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programing
+{
+    class ArithmaticOperators
+    {
+        static void Main()
+        {
+            int a = 111 + 222;
+            Console.WriteLine(a);
+
+            int b = a - 100;
+            Console.WriteLine(b);
+
+            int c = b * 10;
+            Console.WriteLine(c);
+
+            double d = c / 6.3;
+            Console.WriteLine(d);
+
+            Console.WriteLine($"22/7={22 / 7}({22 % 7})");
+        }
+    }
+}
