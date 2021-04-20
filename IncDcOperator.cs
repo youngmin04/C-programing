0c94edc0f9612d8a24e045e1d6658fa92324591b
youngmin04 <s20067@gsm.hs.kr>
2021-04-21 07:40:30 +0900

    C#part35

---
+++ IncDcOperator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programing
+{
+    class IncDcOperator
+    {
+        static void Main()
+        {
+            int a = 10;
+            Console.WriteLine(a++);
+            Console.WriteLine(++a);
+
+            Console.WriteLine(a--);
+            Console.WriteLine(--a);
+        }
+    }
+}
