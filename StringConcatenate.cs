8700d2083b630673e3e8dc9be7307a367bfbf9a6
youngmin04 <s20067@gsm.hs.kr>
2021-04-22 07:40:33 +0900

    C#part36

---
+++ StringConcatenate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programing
+{
+    class StringConcatenate
+    {
+        static void Main()
+        {
+            string result = "123" + "456";
+            Console.WriteLine(result);
+
+            result = "Hello" + "World";
+            Console.WriteLine(result);
+        }
+    }
+}
