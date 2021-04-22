7e4a011c7e1d590a4f9261ad19c8b9fe1e5e703b
youngmin04 <s20067@gsm.hs.kr>
2021-04-23 07:41:26 +0900

    C#part37

---
+++ RelationalOperator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Console;
+
+namespace Programing
+{
+    class RelationalOperator
+    {
+        static void Main()
+        {
+            WriteLine($"3>4:{3>4}");
+            WriteLine($"3>=4:{3 >= 4}");
+            WriteLine($"3<4:{3 < 4}");
+            WriteLine($"3<=4:{3 <= 4}");
+            WriteLine($"3==4:{3 == 4}");
+            WriteLine($"3!=4:{3 != 4}");
+        }
+    }
+}
