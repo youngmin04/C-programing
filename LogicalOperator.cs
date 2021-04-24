a5921c693e7020fb9dd1fef4f0a6c8ce1987c6b7
youngmin04 <s20067@gsm.hs.kr>
2021-04-24 21:04:47 +0900

    C#part38

---
+++ LogicalOperator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Console;
+
+namespace Programing
+{
+    class LogicalOperator
+    {
+        static void Main()
+        {
+            WriteLine("Testing&&...");
+            WriteLine($"1>0&&4<5:{1 > 0 && 4 < 5}");
+            WriteLine($"1>0&&4>5:{1 > 0 && 4 > 5}");
+            WriteLine($"1==0&&4>5:{1 == 0 && 4 > 5}");
+            WriteLine($"1==0&&4<5:{1 == 0 && 4 < 5}");
+
+            WriteLine("\nTesting||...");
+            WriteLine($"1>0||4<5:{1 > 0 || 4 < 5}");
+            WriteLine($"1>0||4>5:{1 > 0 || 4 > 5}");
+            WriteLine($"1==0||4>5:{1 == 0 || 4 > 5}");
+            WriteLine($"1==0||4<5:{1 == 0 || 4 < 5}");
+
+            WriteLine("\nTesting !...");
+            WriteLine($"!True:{!true}");
+            WriteLine($"!False:{!false}");
+        }
+    }
+}
