ed31c6446d7b228b55c6504f0fe58a3e674cc8ea
youngmin04 <s20067@gsm.hs.kr>
2021-04-26 16:44:36 +0900

    C#prat39

---
+++ ConditionalOperator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programing
+{
+    class ConditionalOperator
+    {
+        static void Main()
+        {
+            string result = (10 % 2) == 0 ? "짝수" : "홀수";
+            Console.WriteLine(result);
+        }
+    }
+}
