65fbbecdc6185acc760f65ef099818ab87c08765
youngmin04 <s20067@gsm.hs.kr>
2021-04-28 07:38:20 +0900

    C#part41

---
+++ NullConditionalOperator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+using static System.Console;
+
+namespace Programing
+{
+    class NullConditionalOperator
+    {
+        static void Main()
+        {
+            ArrayList a = null;
+            a?.Add("야구");
+            a?.Add("축구");
+            WriteLine($"Count : {a?.Count}");
+            WriteLine($"{a?[0]}");
+            WriteLine($"{a?[1]}");
+
+            a = new ArrayList();
+            a?.Add("야구");
+            a?.Add("축구");
+            WriteLine($"Count : {a?.Count}");
+            WriteLine($"{a?[0]}");
+            WriteLine($"{a?[1]}");
+        }
+    }
+}
