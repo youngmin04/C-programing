e59467a6cb5da9e10f46931da1e5b9aff0b7448d
youngmin04 <s20067@gsm.hs.kr>
2021-05-06 07:43:45 +0900

    C#part44

---
+++ NullCoalescing.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programing
+{
+    class NullCoalescing
+    {
+        static void Main(string[] args)
+        {
+            int? num = null;
+            Console.WriteLine($"{num ?? 0}");
+
+            num = 99;
+            Console.WriteLine($"{num ?? 0}");
+
+            string str = null;
+            Console.WriteLine($"{str ?? "Default"}");
+
+            str = "Specific";
+            Console.WriteLine($"{str ?? "Default"}");
+        }
+    }
+}
