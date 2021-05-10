3c25560e95e15381ff5f5f52f5dec9a3960966e4
youngmin04 <s20067@gsm.hs.kr>
2021-05-11 08:00:32 +0900

    C#part46

---
+++ SwitchEXP.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programing
+{
+    class SwitchEXP
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("점수를 입력하세요.");
+            int score = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("재수강인가요? (y/n)");
+            string line = Console.ReadLine();
+            bool reapeated =line == "y" ? true : false;
+
+            string grade = (int)(Math.Truncate(score / 10.0) * 10) switch
+            {
+                90 when reapeated == true => "B+",
+                90 => "A",
+                80 => "B",
+                70 => "C",
+                60 => "D",
+                _ => "F"
+            };
+
+            Console.WriteLine($"학점 : {grade}");
+        }
+    }
+}
