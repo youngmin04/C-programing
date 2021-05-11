0929e02af02bf55846169431aa6c4d563b5647f2
youngmin04 <s20067@gsm.hs.kr>
2021-05-12 07:42:52 +0900

    C#part47

---
+++ While.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programing
+{
+    class While
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+            while (i > 0)
+            {
+                Console.WriteLine($"i : {i--}");
+            }
+        }
+    }
+}
