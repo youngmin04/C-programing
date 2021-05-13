08e00c530dafe923393bbf7bf04234a705b37cb4
youngmin04 <s20067@gsm.hs.kr>
2021-05-14 07:39:24 +0900

    C#part49

---
+++ For.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programing
+{
+    class For
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
