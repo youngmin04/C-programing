17a59dd3db0a24b54f751a449f7c708c263c836d
youngmin04 <s20067@gsm.hs.kr>
2021-05-17 07:43:01 +0900

    C#part46

---
+++ ForFor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programing
+{
+    class ForFor
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 5; i++)
+            {
+                for(int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
