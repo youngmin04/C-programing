083035729cafa24a64628be5297703e797ae1beb
youngmin04 <s20067@gsm.hs.kr>
2021-05-18 07:45:11 +0900

    C#part51

---
+++ ForEach.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programing
+{
+    class ForEach
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 0, 1, 2, 3, 4 };
+
+            foreach(int a in arr)
+            {
+                Console.WriteLine(a);
+            }
+        }
+    }
+}
