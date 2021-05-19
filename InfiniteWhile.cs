b97b561d0d5eb4b61d3ee7243001411c81cd095b
youngmin04 <s20067@gsm.hs.kr>
2021-05-19 19:36:14 +0900

    C#part51

---
+++ InfiniteWhile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programing
+{
+    class InfiniteWhile
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while (true)
+            {
+                Console.WriteLine(i++);
+            }
+        }
+    }
+}
