e47750154da96acdd7016fc31ba7ede97ba1cf9c
youngmin04 <s20067@gsm.hs.kr>
2021-05-24 07:40:47 +0900

    C#part52

---
+++ Break.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programing
+{
+    class Break
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("계속 할까요?");
+                string answer = Console.ReadLine();
+
+                if (answer == "아니요")
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
