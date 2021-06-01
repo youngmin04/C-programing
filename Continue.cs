bbf4683f640902b8aed1ef51a78a1d92eed5073e
youngmin04 <s20067@gsm.hs.kr>
2021-06-02 07:42:10 +0900

    C#part53

---
+++ Continue.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programing
+{
+    class Continue
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 10; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    continue;
+                }
+                Console.WriteLine($"{i} : 홀수");
+            }
+        }
+    }
+}
