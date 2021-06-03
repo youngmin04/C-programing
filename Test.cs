c55f51617e1222f5a70e31374458c1cc9e20e123
youngmin04 <s20067@gsm.hs.kr>
2021-06-03 21:12:10 +0900

    C#part55

---
+++ Test.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programing
+{
+    class Test
+    {
+        static void Main()
+        {
+            int num = 5;
+            for (int i = 0; i < num; i++)
+            {
+                for (int j = num - 1; j > i; j--)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int j = 0; j < 2 * i + 1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
