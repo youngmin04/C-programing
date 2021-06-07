1df024ef5cd8aa5a173543c4ddf45ce80c3910ac
youngmin04 <s20067@gsm.hs.kr>
2021-06-07 21:16:02 +0900

    C#part57

---
+++ Test02.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programing
+{
+    class Test02
+    {
+        static void Main()
+        {
+            int[] arr = { 10, 20, 30, 40, 50 };
+            int min = arr[0];
+            int max = arr[0];
+            get_max_min(arr, ref min, ref max);
+            Console.WriteLine(min);
+            Console.WriteLine(max);
+        }
+        private static void get_max_min(int[] arr,ref int min,ref int max)
+        {
+            for(int i = 0; i < 5; i++)
+            {
+                if (min > arr[i])
+                {
+                    min = arr[i];
+                }
+                if (max < arr[i])
+                {
+                    max = arr[i];
+                }
+            }
+        }
+    }
+}
