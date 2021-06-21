fc5b6aa8d91a7fed5bda23d006a23018c601cdd0
youngmin04 <s20067@gsm.hs.kr>
2021-06-21 20:24:36 +0900

    C#RPG2

---
+++ Creature2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programing
+{
+    public enum CreatureType2
+    {
+        None,
+        Player,
+        Monster
+    }
+
+    class Creature2
+    {
+        CreatureType2 type;
+        public int fullhp;
+        public int hp;
+        public int attack;
+        public int coin;
+
+        public Creature2(CreatureType2 type)
+        {
+            this.type = type;
+        }
+        public void SetInfo(int fullhp,int hp,int attack,int coin)
+        {
+            this.fullhp = fullhp;
+            this.hp = hp;
+            this.attack = attack;
+            this.coin = coin;
+        }
+        public int GetHp() { return hp; }
+        public int GetAttack() { return attack; }
+        public bool Death() { return hp <= 0; }
+        public void Damage(int damage)
+        {
+            hp -= damage;
+            if (hp <= 0)
+                hp = 0;
+        }
+    }
+}
