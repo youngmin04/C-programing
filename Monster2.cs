c57fd3cdfa3d41b465d201cdb8f0fb6a65f2a289
youngmin04 <s20067@gsm.hs.kr>
2021-06-23 21:07:06 +0900

    C# RPG2

---
+++ Monster2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programing
+{
+    public enum MonsterType2
+    {
+        None,
+        Ramen,
+        Noodle,
+        Cola
+    }
+    class Monster2 : Creature2
+    {
+        protected MonsterType2 type = MonsterType2.None;
+
+        public Monster2(MonsterType2 type) : base(CreatureType2.Monster)
+        {
+            this.type = type;
+        }
+        public MonsterType2 GetMonsterType() { return type; }
+    }
+    class Ramen : Monster2
+    {
+        public Ramen() : base(MonsterType2.Ramen)
+        {
+            SetInfo(30,30, 5,10);
+        }
+    }
+    class Noodle : Monster2
+    {
+        public Noodle() : base(MonsterType2.Noodle)
+        {
+            SetInfo(20, 20, 10, 15);
+        }
+    }
+    class Cola : Monster2
+    {
+        public Cola() : base(MonsterType2.Cola)
+        {
+            SetInfo(40,40, 15, 20);
+        }
+    }
+}
