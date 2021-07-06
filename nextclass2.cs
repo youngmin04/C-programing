6a989d6bb269b04b8e51e9c6d14150e95eed326c
youngmin04 <s20067@gsm.hs.kr>
2021-07-06 20:41:27 +0900

    C#part63

---
+++ nextclass2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programing
+{
+    class knight
+    {
+        public int hp;
+        public int attack;
+
+        public knight()
+        {
+            hp = 100;
+            attack = 10;
+            Console.WriteLine("생성자 호출");
+        }
+        public knight(int hp) : this()
+        {
+            this.hp = hp;
+            Console.WriteLine("int 생성자 호출");
+        }
+        public knight Clone()
+        {
+            knight knight = new knight();
+            knight.hp = hp;
+            knight.attack = attack;
+            return knight;
+        }
+        public void Move()
+        {
+            Console.WriteLine("knight Move");
+        }
+        public void Attack()
+        {
+            Console.WriteLine("knight Attack");
+        }
+    }
+    class test
+    {
+        static void Main()
+        {
+            knight knight = new knight(50);
+        }
+    }
+}
