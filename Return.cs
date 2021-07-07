0f92019818b65ed63abeae308bac10a2258e3473
youngmin04 <s20067@gsm.hs.kr>
2021-07-07 21:16:24 +0900

    C#part63

---
+++ Return.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programing
+{
+    class Return
+    {
+        static int Fibonacci(int n)
+        {
+            if (n < 2)
+                return n;
+            else
+                return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+        static void PrintProfile(string name,string phone)
+        {
+            if(name=="")
+            {
+                Console.WriteLine("이름을 입력해주세요 : ");
+                return;
+            }
+            Console.WriteLine($"Name : {name},Phone : {phone}");
+        }
+        static void Main()
+        {
+            Console.WriteLine($"10번째 피보나치 수 : {Fibonacci(10)}");
+
+            PrintProfile("","123 - 4567");
+            PrintProfile("박상현", "456-1230");
+        }
+    }
+}
