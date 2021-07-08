683949a819608346cb6713a4b5bc4b2537cfa8ef
youngmin04 <s20067@gsm.hs.kr>
2021-07-08 21:04:51 +0900

    C#part64

---
+++ SwapByValue.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programing
+{
+    class SwapByValue
+    {
+        public static void Swap(int a, int b)
+        {
+            int temp = b;
+            b = a;
+            a = temp;
+        }
+
+        static void Main()
+        {
+            int x = 3;
+            int y = 4;
+
+            Console.WriteLine($"x : {x}, y : {y}");
+
+            Swap(x, y);
+
+            Console.WriteLine($"x : {x}, y : {y}");
+        }
+    }
+}
