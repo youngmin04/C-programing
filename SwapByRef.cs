85569c62824029d7ae2f079c4cc33b9118fe7f19
youngmin04 <s20067@gsm.hs.kr>
2021-07-12 20:17:57 +0900

    C#part65

---
+++ SwapByRef.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programing
+{
+    class SwapByRef
+    {
+        public static void Swap(ref int a,ref int b)
+        {
+            int temp = b;
+            b = a;
+            a = temp;
+        }
+
+        static void Main()
+        {
+            int x = 3;
+            int y = 4;
+
+            Console.WriteLine($"x : {x}, y : {y}");
+
+            Swap(ref x,ref y);
+
+            Console.WriteLine($"x : {x}, y : {y}");
+        }
+    }
+}
