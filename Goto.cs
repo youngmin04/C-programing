9d781e35d401eed1160810be7353f7a8731bda52
youngmin04 <s20067@gsm.hs.kr>
2021-07-14 17:57:10 +0900

    C#part66

---
+++ Goto.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programing
+{
+    class Goto
+    {
+        static void Main()
+        {
+            Console.WriteLine("종료 조건(숫자)을 입력하세요.");
+            string input = Console.ReadLine();
+
+            int input_number = int.Parse(input);
+            int exit_number = 0;
+
+            for(int i = 0; i < 2; i++)
+            {
+                for(int j = 0; j < 2; j++)
+                {
+                    for(int k = 0; k < 3; k++)
+                    {
+                        if (exit_number++ == input_number)
+                            goto EXIT_FOR;
+
+                        Console.WriteLine(exit_number);
+                    }
+                }
+            }
+            goto EXIT_PROGRAM;
+
+        EXIT_FOR:
+            Console.WriteLine("\nExit nested for...");
+        EXIT_PROGRAM:
+            Console.WriteLine("\nEXit program...");
+        }
+    }
+}
