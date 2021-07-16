a512a36717361f26333d6ea5a4b6a446859fa5f4
youngmin04 <s20067@gsm.hs.kr>
2021-07-16 21:19:14 +0900

    C#part67

---
+++ UsingOut.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programing
+{
+    class UsingOut
+    {
+        static void Divide(int a,int b,out int quotient, out int remainder)
+        {
+            quotient = a / b;
+            remainder = a % b;
+        }
+        static void Main()
+        {
+            int a = 20;
+            int b = 3;
+            // int c;
+            // int d;
+
+            Divide(a, b, out int c, out int d);
+            Console.WriteLine($"a : {a}, b: {b}, c : {c}, d : {d}")
+;        }
+    }
+}
