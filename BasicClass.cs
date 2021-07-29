bc127f3a175ef35092ef720e4a8d1f540ecdd94f
youngmin04 <s20067@gsm.hs.kr>
2021-07-29 20:36:32 +0900

    C#part73

---
+++ BasicClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programing
+{
+    class Cat
+    {
+        public string Name;
+        public string Color;
+
+        public void Meow()
+        {
+            Console.WriteLine($"{Name} : 야옹");
+        }
+    }
+    class BasicClass
+    {
+        static void Main()
+        {
+            Cat kitty = new Cat();
+            kitty.Color = "하얀색";
+            kitty.Name = "키티";
+            kitty.Meow();
+
+            Console.WriteLine($"{kitty.Name} : {kitty.Color}");
+
+            Cat nero = new Cat();
+            nero.Color = "하얀색";
+            nero.Name = "키티";
+            nero.Meow();
+
+            Console.WriteLine($"{nero.Name} : {nero.Color}");
+        }
+    }
+
+}
