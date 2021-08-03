b7e40d67df6cc7c6314798757bcb64512574b0d6
youngmin04 <s20067@gsm.hs.kr>
2021-08-03 20:35:18 +0900

    C#part74

---
+++ Constructor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programing
+{
+    class Constructor
+    {
+        class cat
+        {
+
+            public cat()
+            {
+                Name = "";
+                Color = "";
+            }
+            public cat(string _Name, string _Color)
+            {
+                Name = _Name;
+                Color = _Color;
+            }
+            public string Name;
+            public string Color;
+
+            public void Meow()
+            {
+                Console.WriteLine($"{Name} : 야옹");
+            }
+            static void Main()
+            {
+                cat kitty = new cat("키티", "하얀색");
+                kitty.Meow();
+                Console.WriteLine($"{kitty.Name} : {kitty.Color}");
+
+                cat nero = new cat("키티", "하얀색");
+                nero.Meow();
+                Console.WriteLine($"{nero.Name} : {nero.Color}");
+            }
+        }
+    }
+}
