7229681dbdd4bcc48fa35fc2e7d448500b16b093
youngmin04 <s20067@gsm.hs.kr>
2021-08-04 20:30:38 +0900

    C#part75

---
+++ StaticField.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programing
+{
+    class Global
+    {
+        public static int count = 0;
+    }
+    class ClassA
+    {
+        public ClassA()
+        {
+            Global.count++;
+        }
+    }
+    class ClassB
+    {
+        public ClassB()
+        {
+            Global.count++;
+        }
+    }
+    class StaticField
+    {
+        static void Main()
+        {
+            Console.WriteLine($"Global.Count : {Global.count}");
+
+            new ClassA();
+            new ClassA();
+            new ClassB();
+            new ClassB();
+
+            Console.WriteLine($"Global.Count : {Global.count}");
+        }
+    }
+}
